c1ee8edf3c9b617920f7d4d62ca94ded2b4ed40e
Grosul Maxim <maximgrosul@gmail.com>
2021-02-01 22:06:11 +0200

    added files to git

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+// 1
+for(let i=0; i < 5; i++){
+    setTimeout(function() {console.log(i), i * 1000});
+}
+// 2
+
+const  arr = Array.from({length:1000}, () => Math.round(Math.random() * 900 + 1))
+const uniqueArr = [... new Set(arr)] ;
+uniqueArr.length;
+// 3
+const arr3 = [1,0,10,'0','1','', NaN, false, null, undefined, 'false', 'true'];
