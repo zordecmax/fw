eb9a6ad328fdc5d87dc848d9312696eb22365811
Grosul Maxim <maximgrosul@gmail.com>
2021-02-01 22:25:48 +0200

    final code review

--- js/app.js
+++ js/app.js
@@ -1,18 +1,12 @@
-
-
-
-
-
 // 1
 for(let i=0; i < 5; i++){
     setTimeout(function() {console.log(i), i * 1000});
 }
 // 2
-
 const  arr = Array.from({length:1000}, () => Math.round(Math.random() * 900 + 1))
 const uniqueArr = [... new Set(arr)] ;
 uniqueArr.length;
-// 3
 
+// 3
 const arr3 = [1,0,10,'0','1','', NaN, false, null, undefined, 'false', 'true'];
 arr3.filter(el => el == true)
